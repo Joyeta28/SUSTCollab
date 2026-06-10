e5e3b4171bdf6b961a938c4360cacf717214566c
Arifa-Ripa <arifaaktherpk32@gmail.com>
2026-06-10 10:34:52 +0600

    analytics page

---
+++ public/analytics/analytics.js
@@ -0,0 +1,96 @@
+const token = localStorage.getItem("token");
+
+let loggedInUserID;
+let allPosts = [];
+
+document.addEventListener("DOMContentLoaded", async () => {
+    if (!token) {
+        window.location.href = "/login/login.html";
+        return;
+    }
+    try {
+        const res = await fetch("/api/user/profile", {
+            method: "GET",
+            headers: {
+                "Authorization": `Bearer ${token}`
+            }
+        });
+
+        if (res.status === 401 || res.status === 403) {
+            window.location.href = "/login/login.html";
+            return;
+        }
+
+        
+
+        const data = await res.json();
+
+        document.getElementById("UserName").innerText =  data.user.full_name;
+
+        const fullName = data.user.full_name;
+
+        const nameParts = fullName.trim().split(" ");
+
+        let initials = "";
+
+        nameParts.forEach(part => {
+            initials += part[0];
+        });
+
+        document.getElementById("sidebarAvatar").innerText = initials.toUpperCase();
+
+        document.getElementById("profile-link").addEventListener("click", () => {
+                window.location.href ="/profile/profile.html";
+        });
+
+        loggedInUserID = data.user.id;
+
+        loadPosts();
+
+    }catch (err) {
+        console.log(err);
+    }
+});
+
+document.getElementById("logout-btn")
+.addEventListener("click", () => {
+
+    const confirmLogout = confirm(
+        "Are you sure you want to logout?"
+    );
+
+    if (!confirmLogout) return;
+
+    localStorage.removeItem("token");
+
+    window.location.href = "/login/login.html";
+});
+
+
+document.getElementById("delete-account")
+.addEventListener("click", async () => {
+
+    const confirmDelete = confirm(
+        "Are you sure you want to delete your account?"
+    );
+
+    if(!confirmDelete) return;
+    try {
+        const res = await fetch("/api/user/delete", {
+            method: "DELETE",
+            headers: {
+                "Authorization": `Bearer ${token}`
+            }
+        });
+
+        const data = await res.json();
+        alert(data.message);
+        localStorage.removeItem("token");
+        window.location.href = "/login/login.html";
+
+    }catch (err) {
+        console.log(err);
+    }
+    document.getElementById("UserName").innerText =
+        data.user.full_name;
+});
